4c90f304d104eb9cc8e87ad6bdfccea73d2d8acd
Nurul Sundarani <sundarani@prisma.io>
2024-07-20 17:45:35 +0530

    Fix

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,13 @@
 // ./app/api/hello/route.ts
-import prisma from "../../database/prisma";
+import { Pool } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "@prisma/client";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
+const prisma = new PrismaClient({ adapter });
 
 export const runtime = "edge";
 
