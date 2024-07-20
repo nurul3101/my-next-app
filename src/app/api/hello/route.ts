1af1594583f8bb87aaad9f4fa61f6388d981457e
Nurul Sundarani <sundarani@prisma.io>
2024-07-20 17:49:33 +0530

    Fix

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -13,19 +13,9 @@ export const runtime = "edge";
 
 export async function GET(request: Request) {
   const quote = await prisma.quote.findFirst({});
-
-  // Check if a quote was found
-  if (!quote) {
-    return new Response(JSON.stringify({ error: "No quote found" }), {
-      status: 404,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  }
-
+  console.log("quote", quote);
   // Respond with the quote in JSON format
-  return new Response(JSON.stringify({ quote: quote.text }), {
+  return new Response(JSON.stringify({ quote: quote?.text }), {
     status: 200,
     headers: {
       "Content-Type": "application/json",
